b2d26252931837a830dea15136b854b4bc39be52
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-02 18:36:01 +0700

    Initialized Laravel + Inertia + React

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Head } from "@inertiajs/inertia-react";
+
+export default function Index() {
+  return (
+    <>
+      <Head title="Home | FileBag" />
+
+      <h1>Welcome to FileBag</h1>
+    </>
+  );
+}
