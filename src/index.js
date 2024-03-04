0136840045fd777bf53039d33d3f014d9128a164
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-04 08:43:35 +0700

    (fix): resolve conflicting index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 import Login from './login'
 import reportWebVitals from './reportWebVitals';
-=======
->>>>>>> ac9fee8be3b236ea0df1cdd74226eb39d8342cf7
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
