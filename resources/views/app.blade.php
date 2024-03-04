c35167715ff98ece5c957d954b72107d5cdc2809
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-04 09:30:23 +0700

    (fix): add new font (Satoshi)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
   @vite('resources/css/app.css')
   @vite('resources/js/app.jsx')
   @inertiaHead
