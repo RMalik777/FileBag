e24cc9737a9bb64ceb620f880cfabf4a58d3e03e
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-04 16:34:03 +0700

    (feat): add new button

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -4,12 +4,26 @@ import Navbar from "../Components/Navbar/Navbar";
 export default function Index() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className="w-full bg-cover bg-fixed bg-[url(../../../public/assets/homecover.jpg)] h-screen">
         <div className="flex flex-col justify-between backdrop-brightness-50 h-full pt-60 ">
-          <div className="flex flex-col px-8">
-            <h1 className="text-6xl text-white font-bold">Project SOP Files</h1>
-            <h2 className="text-2xl text-white">*Maximum 3MB File Size in PDF Format</h2>
+          <div className="px-12 flex flex-row justify-between">
+            <div className="flex flex-col">
+              <h1 className="text-6xl text-white font-bold">
+                Project SOP Files
+              </h1>
+              <h2 className="text-2xl text-white">
+                *Maximum 3MB File Size in PDF Format
+              </h2>
+            </div>
+            <div>
+              <button className="group flex flex-row justify-center text-2xl px-5 py-3 text-white font-semibold rounded-full border border-4 border-white hover:bg-white hover:text-black *:hover:text-black">
+                <span className="material-symbols-rounded pr-4 text-white text-3xl">
+                  cloud_upload
+                </span>
+                Upload
+              </button>
+            </div>
           </div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
