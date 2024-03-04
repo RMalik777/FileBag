b8d6411f4292ae8242956c7fa26895f1d7aefed9
vkholvin <vkholvin@gmail.com>
2024-03-04 10:45:52 +0700

    (fix): Ganti css ke tailwind

--- src/login.js
+++ src/login.js
@@ -1,54 +1,52 @@
-import './login.css';
 import React, { useState, useEffect } from 'react';
-import Poto from './aset/Poto.svg';
-import Poto1 from './aset/Poto1.svg';
-import Poto2 from './aset/Poto2.svg';
-import filexCIMB from './aset/filexCIMB.png'
+import Poto from './aset/Poto.png';
+import Poto1 from './aset/Poto1.png';
+import Poto2 from './aset/Poto2.png';
+import filexCIMB from './aset/filexCIMB.png';
 
 const Login = () => {
-        const photos = [Poto, Poto1, Poto2];
-        const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(0);
-      
-        useEffect(() => {
-          // Start an interval to automatically change the photo every 3 seconds
-          const intervalId = setInterval(() => {
-            setSelectedPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
-          }, 3000);
-      
-          // Clean up the interval on component unmount
-          return () => clearInterval(intervalId);
-        }, [photos.length]);
-        
-        return(
-        <div className='container'>
-        <div className='image-side' style={{ backgroundImage: `url(${Poto})` }}>
-            <div className='CenterPicture'>
-            <img src={filexCIMB} className='fileXCIMB'/>
-            </div>
-        </div>
-        <div className='form-side'>
-          <h2>Login Page</h2>
-          <img src={filexCIMB} className='fileXCIMB'/>
-          <form>
-            <div className='form-group'>
-              <label htmlFor='name'>Username</label>
-              <input type='text' id='name' name='name' />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='password'>Password</label>
-              <input type='password' id='password' name='password' />
-            </div>
-            <div className='form-group remember-me'>
-                <input type='checkbox' id='rememberMe' name='rememberMe' />
-                <label htmlFor='rememberMe'>Remember me</label>
-            </div>
-            <button type='submit'><strong>Login</strong></button>
+  const photos = [Poto, Poto1, Poto2];
+  const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(0);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSelectedPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [photos.length]);
+
+  return (
+    <div className="flex h-screen">
+      <div className="flex-[45%] bg-cover bg-center  transition-all duration-1000" style={{ backgroundImage: `url(${photos[selectedPhotoIndex]})` }}>
+        <div className="flex flex-col items-center backdrop-brightness-50 justify-center h-full">
+          <img src={filexCIMB} alt="Company Logo" className="w-[380px] mb-8 justify-center" />
+          <h2 className='text-white text-[20px] justify-center'>Your Number 1 File Management System</h2>
+        </div>
+      </div>
+      <div className="flex-[55%] flex items-center justify-center bg-red-800">
+        <div className="w-full max-w-md p-8">
+          <h2 className="text-white font-semibold text-3xl mb-8 text-center">Login Page</h2>
+          <img src={filexCIMB} alt="Company Logo" className="w-[300px] mb-8 mx-auto" />
+          <form>
+            <div className="mb-4">
+              <label htmlFor="name" className="text-white block">Username</label>
+              <input type="text" id="name" name="name" className="w-full border border-gray-300 rounded-md p-2" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="text-white block">Password</label>
+              <input type="password" id="password" name="password" className="w-full border border-gray-300 rounded-md p-2" />
+            </div>
+            <div className="mb-4 flex items-center">
+              <input type="checkbox" id="rememberMe" name="rememberMe" className="rounded-full cursor-pointer mr-2" />
+              <label htmlFor="rememberMe" className="text-white">Remember me</label>
+            </div>
+            <button type="submit" className="w-full bg-white text-black font-bold py-2 rounded-md hover:bg-red-900">Login</button>
           </form>
         </div>
       </div>
-  
-    )
+    </div>
+  );
 };
 
 export default Login;
