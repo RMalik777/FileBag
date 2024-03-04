a2561d9224c548bd6647845f73c722364431cee4
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-04 11:38:05 +0700

    (fix): fix page routing

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index']);
-Route::get('upload', [PageController::class, 'upload']);
-Route::get('login', [PageController::class, 'login']);
+Route::get('/upload', [PageController::class, 'upload']);
+Route::get('/login', [PageController::class, 'login']);
