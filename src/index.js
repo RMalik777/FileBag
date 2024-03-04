b9d92ce665b61f69fac5857933e6e4c48e2e0677
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-04 10:06:48 +0700

    (fix): remove webvitals import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Login from './login'
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
