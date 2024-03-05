84790b2a934b583237dfa09702bc28f0ac540b10
vkholvin <vkholvin@gmail.com>
2024-03-05 08:31:28 +0700

    (fix): design search bar

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -39,7 +39,7 @@ export default function Index() {
       </div>
       <div className="h-screen">
             <div className="flex">
-            <select id="category" name="category" defaultValue="category" className="bg-3 font-bold text-white w-36 h-10 text-center mb-6">
+            <select id="category" name="category" defaultValue="category" className="bg-3 font-bold rounded- text-white w-36 h-10 text-center mb-6">
                 <option className="hidden" selected>Category</option>
                 <option value={1}>IT</option>
                 <option value={2}>HR</option>
@@ -47,10 +47,16 @@ export default function Index() {
                 <option value={4}>L&T</option>
             </select>
             </div>
-            <div className="flex flex-row justify-between mx-">
+            <div className="flex flex-row justify-between">
                 <p>Choose the Following Category Below!</p>
-                <form>
-                    <input type="text" placeholder="Search" className="bg-3 text-white" />
+                <form className='relative pm'>
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>    
+                <input type="search" id="default-search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 
+                rounded-xl bg-3 text-white placeholder-white" placeholder="Search" required />
 
                 </form>
             </div>
