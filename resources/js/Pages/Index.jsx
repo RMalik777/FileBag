7ad29447c12806fb36b775c5e371fef4aec636db
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-06 15:18:36 +0700

    (fix): fix index cant be run

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -147,7 +147,6 @@ export default function Index(props) {
                   <span className="material-symbols-rounded text-3xl hover:material-fill hover:text-cimbred">
                     schedule
                   </span>
-                </span>
                   {/* Replace with plus icon code */}
                   <span className="material-symbols-rounded text-3xl hover:material-fill hover:text-cimbred">
                     add_circle
