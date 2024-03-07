cf60b69d8d1e4d044ac1919180a7d34b7ae63138
Rafli Malik <Rafli.Malik@gmail.com>
2024-03-07 10:04:56 +0700

    (fix): fix table data with new query

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -19,34 +19,39 @@ class PageController extends Controller
   public function index(Request $request)
   {
     $searchword = $request->searchword;
-    if(strlen($searchword)){
-      $fileDetail = FileDetail::where('file_name','like','%'. $searchword .'%')->get();
-      if($fileDetail){
+    if (strlen($searchword)) {
+      $fileDetail = FileDetail::where('file_name', 'like', '%' . $searchword . '%')->get();
+      if ($fileDetail) {
         $fileHeaderIds = $fileDetail->pluck('id');
         $fileHeaderShow = FileHeader::whereIn('file_detail_id', $fileHeaderIds)->get();
-      }
-      else{
+      } else {
         $fileHeaderShow = FileHeader::all()->get();
       }
-    }
-    else{
-      $fileHeaderShow = FileHeader::all();  
+    } else {
+      $fileHeaderShow = FileHeader::all();
     }
     $fileHeader = FileHeader::all();
     $fileDetail = FileDetail::all();
     $category = Category::all();
     $users = User::all();
-        
-    if (Auth::user()){
+    $data = DB::table(DB::raw('(SELECT fh.*, ROW_NUMBER() OVER (PARTITION BY fh.category_id ORDER BY fh.version DESC) AS rn FROM file_headers fh) as rc'))
+      ->select('rc.id', 'rc.version', 'rc.file_date', 'rc.category_id', 'rc.user_id', 'u.username', 'c.category_name', 'fd.file_name')
+      ->join('categories as c', 'rc.category_id', '=', 'c.id')
+      ->join('file_details as fd', 'rc.file_detail_id', '=', 'fd.id')
+      ->join('users as u', 'rc.user_id', '=', 'u.id')
+      ->where('rc.rn', 1)
+      ->get();
+
+    if (Auth::user()) {
       return Inertia::render('Index', [
         'fileHeader' => $fileHeader,
         'fileDetail' => $fileDetail,
         'category' => $category,
         'users' => $users,
-        'show' => $fileHeaderShow
+        'show' => $fileHeaderShow,
+        'data' => $data,
       ]);
-    }
-    else{
+    } else {
       return redirect('/login');
     }
   }
--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -3,22 +3,15 @@ import Navbar from "../Components/Navbar/Navbar";
 import { Head, usePage } from "@inertiajs/react";
 
 export default function Index(props) {
-  
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(2); //per page nampilin brp kolom (ini valuenya 2 buat ngetes aja karena data cuma 4)
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  
   //data buat tabel
-  let header = props.fileHeader;
-  let detail = props.fileDetail;
-  let category = props.category;
-  let users = props.users;
-  let result = props.show;
-  if(window.location.href.includes("?searchword=")){
-    header = result;
-  }
+  const header = props.fileHeader;
+  const detail = props.fileDetail;
+  const category = props.category;
+  const users = props.users;
+  const data = props.data;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(4); //per page mau nampilin brp kolom (disesuaiin)
 
   const totalPages = Math.ceil(header.length / itemsPerPage);
 
@@ -26,15 +19,10 @@ export default function Index(props) {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
   
+
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-
-  useEffect(() => {
-    const slicedData = header.slice(indexOfFirstItem, indexOfLastItem);
-    setSearchResults(slicedData); // Pass the sliced data to searchResults
-  }, [currentPage, header, itemsPerPage]);
-
   return (
     <>
       <Head title="Dashboard | Filebag" />
@@ -101,16 +89,15 @@ export default function Index(props) {
               </option>
             </select>
           </div>
-          <form className="relative w-1/3" onSubmit={fetch(`/?searchword=${searchTerm}`)}>
+          <form className="relative w-1/3">
             <div className="w-full">
               <input
                 type="search"
                 id="default-search"
                 className="block w-full pl-16 pr-8 py-4 text-lg font-medium border border-gray-300 rounded-full bg-cimbred text-white placeholder-gray-100"
-                placeholder="Search"
                 name="searchword"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
+                // value="{{ Request::get('searchword') }}"
+                placeholder="Search"
                 required
               />
               <span className="material-symbols-rounded absolute ml-2 inset-y-0 start-0 flex items-center ps-5 pointer-events-none text-gray-100">
@@ -140,20 +127,20 @@ export default function Index(props) {
             </tr>
           </thead>
           <tbody className="bg-white text-black">
-            {searchResults.map((item, index) => (
+            {data.map((item, index) => (
               <tr
                 key={item.id}
                 className="border-cimbred border-x-0 border-y-2">
                 <td className="py-3">
                   <span className="duration-300 ease-out">
-                    {detail[item.id - 1]?.file_name}
+                    {item.file_name}
                   </span>
                 </td>
                 <td className="py-3 ">
-                  {category[item.id - 1]?.category_name}
+                  {item.category_name}
                 </td>
                 <td className="py-3 text-center">{item.file_date}</td>
-                <td className="py-3 text-center">{users[item.id-1]?.username}</td>
+                <td className="py-3 text-center">{item.username}</td>
                 <td className="text-center *:px-1 *:text-4xl">
                   <span className="material-symbols-rounded hover:material-fill hover:text-cimbred cursor-pointer">
                     schedule
